b6ff058fdfbb2fdd1e5e8f61fff80852e6ef97a2
domad007 <d.fiedorczuk@students.ephec.be>
2018-11-04 18:53:04 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Controlleur/Movement.java
    #	src/Vue/VueConsole.java

--- src/Controlleur/Movement.java
+++ src/Controlleur/Movement.java
@@ -13,6 +13,9 @@ public class Movement {
             case "q":
                 left();
                 break;
+            case "d":
+                right();
+                break;
         }
     }
 
@@ -25,6 +28,28 @@ public class Movement {
                     Grille.getGrille()[i][j] = 0;
                     int newPos = j-1;
                     Grille.getGrille()[i][newPos] =1 ;
+                }
+                else if(Grille.getGrille()[9][0] == 1){
+                    Grille.getGrille()[9][0] = 1;
+                    return;
+                }
+
+            }
+        }
+    }
+    public void right(){
+
+        for(int i =0; i<10; i++){
+            for(int j = 0; j<10; j++){
+                if(Grille.getGrille()[i][j] == 1){
+                    Grille.getGrille()[i][j] = 0;
+                    int newPos = j+1;
+                    Grille.getGrille()[i][newPos] =1 ;
+                    return;
+                }
+                else if(Grille.getGrille()[i][9] == 1){
+                    Grille.getGrille()[i][9] = 1;
+                    return;
                 }
 
             }
--- src/Vue/VueConsole.java
+++ src/Vue/VueConsole.java
@@ -6,11 +6,11 @@ public class VueConsole {
     public static void main(String[] args){
         Movement move = new Movement();
         Grille gri = new Grille();
-        gri.initialise(gri.getGrille());
-        gri.getGrille()[8][5] = 1;
-        gri.affiche(gri.getGrille());
-         move.move();
-        move.left();
-        gri.affiche(gri.getGrille());
+        gri.getGrille()[9][4] = 1;
+
+        do {
+            gri.affiche(gri.getGrille());
+            move.move();
+        } while(true);
     }
 }
