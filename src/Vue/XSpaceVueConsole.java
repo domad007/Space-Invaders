a17bd7dc3aad906b3e098db31b4d9c18338b3244
domad007 <d.fiedorczuk@students.ephec.be>
2018-11-08 14:57:06 +0100

    MVC

---
+++ src/Vue/XSpaceVueConsole.java
@@ -0,0 +1,37 @@
+package Vue;
+
+import Controlleur.Monster;
+import Controlleur.Vaisseau;
+import Modele.Grille;
+
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Scanner;
+
+public class XSpaceVueConsole extends XSpaceVue implements Observer {
+    Scanner scanner;
+
+
+    public XSpaceVueConsole(Grille model, Vaisseau controllerVaiss, Monster controllerMonst){
+        super(model, controllerVaiss, controllerMonst);
+        play();
+    }
+
+    public void update(Observable o, Object arg){
+       System.out.println(model.getGrille());
+
+    }
+
+    public void play() {
+
+        model.initialise(model.getGrille());
+        model.getGrille()[9][4] = 1;
+        controllerMonst.generateMonster();
+
+        do {
+            model.affiche(model.getGrille());
+            controllerVaiss.moveVaiss();
+            controllerMonst.monsterDown();
+        }while(true);
+    }
+}
