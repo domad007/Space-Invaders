fe9556050dca98bf8b95792f2a8de1020beef58c
Adelin Gauthier <a2l195@hotmail.com>
2018-12-21 14:42:21 +0100

    Update Client.java

--- XSpaceMVC/src/socket/Client.java
+++ XSpaceMVC/src/socket/Client.java
@@ -1,14 +1,47 @@
 package socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * L'objet socket a besoin de l'IP et du Port du ServerSocket
+		 */
+                Socket client = null;
+		try {
+			client = new Socket("localhost", 11111);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			System.exit(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		/*
+		 * Création des objets permettant de communiquer avec le client
+		 */
+		PrintStream output = null;
+		BufferedReader input = null;
+		try {
+			output = new PrintStream(client.getOutputStream());
+			input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		/*
+		 * Boucle principale
+		 */
+		while (true) {
+			
+		}
 	}
-
 }
