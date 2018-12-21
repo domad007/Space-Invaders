46f64aa1f6b6de0f8bdcceb870a3000e66dd3dc0
Adelin Gauthier <a2l195@hotmail.com>
2018-12-21 13:17:30 +0100

    Socket

--- XSpaceMVC/src/socket/Client.java
+++ XSpaceMVC/src/socket/Client.java
@@ -1,5 +1,9 @@
 package socket;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
 
 	public static void main(String[] args) {
--- XSpaceMVC/src/socket/Serveur.java
+++ XSpaceMVC/src/socket/Serveur.java
@@ -1,10 +1,55 @@
 package socket;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Serveur {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * Création du serveur
+		 */
+		ServerSocket serversocket = null;
+		try {
+			serversocket = new ServerSocket(11111);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		try {
+			Socket client = serversocket.accept();
+			System.out.println("Un client s'est connecté");
+			
+			/*
+			 * Création des objets permettant de communiquer avec le client
+			 */
+			BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			PrintStream output = new PrintStream(client.getOutputStream());
+			
+			/*
+			 * Note : On peut utiliser l'objet scanner au lieu du BufferedReader
+			 */
+			Scanner inputScanner = new Scanner(client.getInputStream());
+			
+			/*
+			 * La communication se fera dans cette boucle
+			 */
+			
+			/*
+			 * La communication se fera dans cette boucle
+			 */
+			while (true) {
+				
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
